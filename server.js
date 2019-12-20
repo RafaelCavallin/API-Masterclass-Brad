fac240b54a8eb873677b42d1d1101a8e81361bc4
RafaelCavallin <rafael_cavallin@hotmail.com>
2019-12-19 22:55:34 -0300

    Creating CRUD for bootcamps

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-// Route files
-const bootcamps = require("./routes/bootcamps");
+// Load Logger
+//const logger = require("./middleware/logger");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Conect to DB
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
 
-// Mount ruoters
+// Dev loggin middelware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
+
+//5. Create Bootcamp - POST
